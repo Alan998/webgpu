049ee22620584db929618055e78283db48a4d5eb
alan <alanfm61226@gmail.com>
2025-04-14 09:48:54 +0800

    add code to draw red triangle hardcoded

--- src/draw_triangle.js
+++ src/draw_triangle.js
@@ -1,8 +1,13 @@
+// function to log messages when failing
+function fail(msg) {
+	alert(msg);
+}
+
 // WebGPU is an asynchrounous API, so use it in an async function
 async function main() {
 	// request an adapter
 	const adapter = await navigator.gpu?.requestAdapter();
-	
+
 	// request GPU device from the adapter
 	const device = await adapter?.requestDevice();
 
@@ -13,6 +18,92 @@ async function main() {
 	} else {
 		console.log('Browser supports WebGPU, Yay!');
 	}
+
+	// Get a WebGPU context from the canvas and configure it
+	const canvas = document.querySelector('canvas');
+	const context = canvas.getContext('webgpu');
+	// ask the system for preferred format, either "rgba8unorm" or "bgra8unorm"
+	const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+	context.configure({
+		device, // associate device with canvas
+		format: presentationFormat,
+	});
+
+	// Create a shader module which contains a vertex shader and a fragment shader
+	const module = device.createShaderModule({
+		label: 'our hardcoded red triangle shaders',
+		code: `
+			@vertex fn vs(
+				@builtin(vertex_index) vertexIndex: u32
+			) -> @builtin(position) vec4f {
+				let pos = array(
+					vec2f( 0.0,  0.5), // top center
+					vec2f(-0.5, -0.5), // bottom left
+					vec2f( 0.5, -0.5), // bottom right
+				);
+
+				return vec4f(pos[vertexIndex], 0.0, 1.0);
+			}
+
+			@fragment fn fs() ->@location(0) vec4f {
+				return vec4f(1.0, 0.0, 0.0, 1.0);
+			}
+		`
+	})
+
+	// Create a render pipeline
+	const pipeline = device.createRenderPipeline({
+		label: 'our hardcoded red triangle pipeline',
+		// set layout to 'auto' means to ask Webgpu to derive the layout of
+		// data from the shaders, but we are not using any data for now
+		layout: 'auto',
+		vertex: {
+			entryPoint: 'vs',
+			module,
+		},
+		fragment: {
+			entryPoint: 'fs',
+			module,
+			targets: [{ format: presentationFormat }],
+		},
+	});
+
+	// Prepare a GPURenderPassDescriptor which describes which textures we want
+	// to draw to and how to use them
+	const renderPassDescriptor = {
+		label: 'our basic canvas renderPass',
+		colorAttachments: [
+			{
+				// view: <- to be filled out when we render
+				clearValue: [0.3, 0.3, 0.3, 1],
+				loadOp: 'clear',
+				storeOp: 'store',
+			},
+			,]
+	};
+
+
+	function render() {
+		// Get the current texture from the canvas context and set it as the texture
+		// to render to.
+		renderPassDescriptor.colorAttachments[0].view = 
+			context.getCurrentTexture().createView();
+
+		// make a command encoder to start encoding commands
+		const encoder = device.createCommandEncoder({ label: 'our encoder' });
+
+		// make a render pass encoder to encode render specific commands
+		const pass = encoder.beginRenderPass(renderPassDescriptor);
+		pass.setPipeline(pipeline);
+		pass.draw(3); // call our vertex shader 3 times
+		pass.end();
+
+		const commandBuffer = encoder.finish();
+		device.queue.submit([commandBuffer]);
+	}
+
+	render();
 }
+
 
 main();
